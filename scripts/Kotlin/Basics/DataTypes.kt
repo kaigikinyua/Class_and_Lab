8c596cc0f0e8d8b6dac9569ae4348b3b2e5593af
kaigikinyua <kaigikinyua@gmail.com>
2020-06-24 20:50:58 +0300

    basis of types

--- scripts/Kotlin/Basics/DataTypes.kt
+++ scripts/Kotlin/Basics/DataTypes.kt
@@ -1,22 +1,23 @@
-class Student(val name:String,val regNumber:String){
-    init{
-        println("Welcome $name")
+class MathOperations(values:Array<Int>){
+    val array=values
+    fun add():Int{
+        var sum:Int=0
+        for (item in array){
+            sum+=item
+        }
+        return sum
     }
-    fun register(){
-        println("Registering $name with regnumber $regNumber")
-    }
-}
-class Teacher(name:String,staffID:String){
-    init(){}
-    fun fetchClasses(){}
-    fun enterStudentData(){}    
+    fun subtraction(){}
+    fun multiply(){}
+    fun division(){}
+    // fun sort(){
+    //     var pivot:Int=values.size/2
+    // }
 }
 
 
 fun main(){
-    var student1=Student("Jim","BBIT-3435")
-    student1.register()
-    var student2=Student("James","CS-3013")
-    student2.register()
-    println(student1.name)
+    var myarray=arrayOf(10,20,30)
+    var opp=MathOperations(myarray)
+    println(opp.add())
 }
