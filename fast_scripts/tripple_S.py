8543be1d56c099671ef72a422177ae9955dc4732
kaigikinyua <kaigikinyua@gmail.com>
2020-08-03 15:42:56 +0300

    Organizing into classes

--- fast_scripts/tripple_S.py
+++ fast_scripts/tripple_S.py
@@ -3,47 +3,58 @@ import os
 import sys
 import re
 
-def fetch_page(url):
-	page=requests.get(url)
-	return page.content
 
-def fetch_static(url):
-	pass
+class WebPage:
+	def __init__(url):
+		self.base_url=url
 
-def save_file(filename,content):
-	try:
-		file_path="./Tests/"+str(filename)
-		file=open(file_path,"w")
-		file.write(content)
-		file.close()
-	except:
-		print(e)
-		print("Error while saving the file")
+	def fetch_page():
+		page=requests.get(self.url)
+		return page.content
 
-def read_file(file_path):
-	data=[]
-	try:
-		f=open(file_path,"r")
-		data=f.readlines()
-		f.close()
-	except:
-		print("Error while reading file")
-		data=False
-	return data
+	def fetch_static_assets():
+		pass
 
-def save_refined_file(file_path):
-	data=read_file(file_path)
-	for line in data:
-		print(line)
+class Save:
 
-def extract_file_name(url):
-	x=url.split('/')
-	return x[len(x)-1]
-def find_reg_expr(expr,search_data):
-	pass
+	def save_file(filename,content):
+		try:
+			file_path="./Tests/"+str(filename)
+			file=open(file_path,"w")
+			file.write(content)
+			file.close()
+		except:
+			print(e)
+			print("Error while saving the file")
+
+	def read_file(file_path):
+		data=[]
+		try:
+			f=open(file_path,"r")
+			data=f.readlines()
+			f.close()
+		except:
+			print("Error while reading file")
+			data=False
+		return data
+
+	def save_refined_file(file_path):
+		data=read_file(file_path)
+		for line in data:
+			print(line)
+
+	def extract_file_name(url):
+		x=url.split('/')
+		return x[len(x)-1]
+
+class RegularExp
+	
+	def find_reg_expr(expr,search_data):
+		pass
 
 if __name__=="__main__":
 	args=sys.argv
-	page_contents=fetch_page(args[1])
+	page=WebPage(args[1])
+	page_contents=page.fetch_page()
 	save_file("index.html",page_contents.decode('utf-8'))
 	save_refined_file("./Tests/index.html")
