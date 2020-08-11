8a6497961b907856cd1d340271d4576aebfaf024
kaigikinyua <kaigikinyua@gmail.com>
2020-08-11 21:52:08 +0300

    Revisit enums

--- C#/Tests/DataStructure.cs
+++ C#/Tests/DataStructure.cs
@@ -4,6 +4,7 @@
 //enums and structs
 using System;
 namespace DataStructures{
+    //structs
     public struct User{
         public string firstname,lastname,password;
         public int id;
@@ -12,6 +13,8 @@ namespace DataStructures{
             Console.WriteLine("Username:{0} {1}\nPassword:{2}\nId:{3}",firstname,lastname,password,id);
         }
     }
+    //enum
+    public enum Constants{TaxRates=3,Bonus,Shares=1}
     class PrimitiveDT{
         public void ArrayFunction(int[] param_array){
             foreach (int item in param_array){
@@ -24,9 +27,12 @@ namespace DataStructures{
             //int[] array={10,10,100};
             //PrimitiveDT dt=new PrimitiveDT();
             //dt.ArrayFunction();
-            User u=new User();
-            u.firstname="James";u.lastname="May";u.password="Ohh Cock!!";u.id=100;
-            u.userDetails();
+            //User u=new User();
+            //u.firstname="James";u.lastname="May";u.password="Ohh Cock!!";u.id=100;
+            //u.userDetails();
+            int t=(int)Constants.TaxRates;
+            Console.WriteLine(t);
+            Console.WriteLine((int)Constants.Bonus);
         }
     }
     
