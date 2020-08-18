d1c4fb8fb609c8d34d70aff7700270f06b5726b4
kaigikinyua <kaigikinyua@gmail.com>
2020-08-18 17:05:16 +0300

    Colored notifications

--- C#/Projects/RocketShip/Utils.cs
+++ C#/Projects/RocketShip/Utils.cs
@@ -2,18 +2,26 @@ using System;
 namespace Utils{
     class Messages{
         public static void error(string message){
-            Console.WriteLine("Error"+message);
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("Error:> "+message);
         }
         public static void success(string message){
+            Console.ForegroundColor=ConsoleColor.Green;
             Console.WriteLine("<----"+message+"---->");
         }
         public static void notification(string message){
+            Console.ForegroundColor=ConsoleColor.White;
             Console.WriteLine(message);
         }
     }
     class Files{
         public static void readFile(string filePath){}
         public static void writeFile(string filePath){}
+        public static void Main(string[] args){
+            Messages.error("Test error message");
+            Messages.success("Testing success message");
+            Messages.notification("Testing print message");
+        }
     }
     
 }
