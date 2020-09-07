0890c45625e0660e386b7da0fa800795a309ce61
kaigikinyua <kaigikinyua@gmail.com>
2020-09-07 16:26:44 +0300

    asp.net

--- C#/Projects/School/ProgramFlow.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Calc;
-class ProgramFlow{
-    static void Main(string[] args){
-        int sum=Calculator.sumTotal();
-        float average=Calculator.average(sum,3);
-        Console.WriteLine("The average is {0}",average);
-    }
-}
--- C#/Tests/Basics/Main.cs
+++
@@ -1,9 +0,0 @@
-
-class MainClass{
-	static void Main(string[] args){
-		System.Console.WriteLine("Hello World");
-		foreach (string arg in args){
-			System.Console.WriteLine(arg);
-		}
-	}
-}
