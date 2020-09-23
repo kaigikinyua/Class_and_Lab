4291177e6486479ae09acb43e43b25e5be53bd78
kaigikinyua <kaigikinyua@gmail.com>
2020-09-23 15:18:34 +0300

    autoscroll success

--- Frontend/frameworks_and_libs/react/react-0.14.3/myprojects/js/dynamic.js
+++ Frontend/frameworks_and_libs/react/react-0.14.3/myprojects/js/dynamic.js
@@ -1,11 +1,21 @@
-var curr_slide="one";var count=1;var width=screen.width
+var curr_slide="one";
+var count=1;
+const width=screen.width
 const slides=["one","two","three","four"]
+var scrolledBy=0
+var max=0.95*((slides.length)*(width/2))
 function autoScroll(){
-    if(count>=slides.length){count=1}
-    else{count+=1}
+    if(scrolledBy>max){scrolledBy=0;count=1}
+    else{
+        scrolledBy=(width/2)*count
+        count+=1
+    }
     //location.href="#"+slides[count]
     var elem=document.querySelector(".slides")
-    elem.scrollLeft=(screen.width/2)*count;
+    elem.scrollLeft=scrolledBy;
     console.log("Hello world this is auto scroll")
 }
-setInterval(autoScroll,7000)
+setInterval(autoScroll,10000)
+
+function slideLeftBy(numberOfPix){}
+function resetScroll(){}
