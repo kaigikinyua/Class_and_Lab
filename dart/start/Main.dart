360d374d63dbff24d66595dc699de4cb0981b027
kaigikinyua <kaigikinyua@gmail.com>
2020-09-23 20:29:30 +0300

    learning lists

--- dart/start/Main.dart
+++ dart/start/Main.dart
@@ -2,6 +2,15 @@ import './IO.dart';
 import './Rocket.dart';
 
 void main(){
-  Rocket r=new Rocket(model: "Model 2",name: "Apollo One");
-  r.ignition();
+  //String rocketSimNum=UserInput.enter(true,"How many rocket simulations would you like to run");
+  List<Rocket> rocketList=[];
+  for(int i=0;i<3;i++){
+    String rName=UserInput.enter(true,"Rocket name");
+    String rModel=UserInput.enter(true,"Rocket model");
+    Rocket r=new Rocket(name: rName,model: rModel);
+    rocketList.insert(i, r);
+  }
+  rocketList.forEach((f)=>{
+    print("Rocket name:"+f.name+" model:"+f.model)
+  });
 }
--- dart/start/Rocket.dart
+++ dart/start/Rocket.dart
@@ -2,23 +2,29 @@ import 'IO.dart';
 
 class Rocket{
   String name;String model;
+  double payloadWeight,fuseladgeWeight,fuelCapacity;
   Rocket({String name,String model}){
-    this.name=name;
-    this.model=model;
-    print("Rocket $name -- model $model");
+    this.name=name;this.model=model;
   }
   void ignition(){
     UserInput.output("<----------Ignition---------->");
   }
-  bool setUpRocket({double payloadWeight,double rocketWeight,double fuelCapacity}){
-    List<double> unEmpty=[payloadWeight,rocketWeight,fuelCapacity];
+  bool setUpRocket({double payloadWeight,double fuseladgeWeight,double fuelCapacity}){
+    List<double> unEmpty=[payloadWeight,fuseladgeWeight,fuelCapacity];
     for (int i=0;i<unEmpty.length;i++){
       if(unEmpty[i]!=null){return false;}
     }
-    
     return true;
   }
   void runDiagnostics(){}
   void launch({String launchDate,String mission,String coordinates}){}
+  //double rocketAcceleration(){}
 
+}
+
+class Physics{
+  static calcWeight({double volume,double density}){return volume*density;}
+  static calcVelocity({double acceleration,double time}){return acceleration*time;}
+  static calcDistanceTraveled({double velocity,double time}){return velocity*time;}
+  //static calcTragectory(){}
 }
