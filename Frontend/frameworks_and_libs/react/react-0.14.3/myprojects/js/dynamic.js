28d3f980c3d1a549412aa20b6c634d442f670e50
kaigikinyua <kaigikinyua@gmail.com>
2020-09-23 15:04:04 +0300

    carousel scroll success[no glitches and no distruption of the dom] bug: how to synchronize the scroll with user input and autoscroll

--- Frontend/frameworks_and_libs/react/react-0.14.3/myprojects/js/dynamic.js
+++ Frontend/frameworks_and_libs/react/react-0.14.3/myprojects/js/dynamic.js
@@ -1,9 +1,11 @@
-var curr_slide="one";var count=0;
+var curr_slide="one";var count=1;var width=screen.width
 const slides=["one","two","three","four"]
 function autoScroll(){
-    if(count>=slides.length-1){count=0}
+    if(count>=slides.length){count=1}
     else{count+=1}
-    location.href="#"+slides[count]
+    //location.href="#"+slides[count]
+    var elem=document.querySelector(".slides")
+    elem.scrollLeft=(screen.width/2)*count;
     console.log("Hello world this is auto scroll")
 }
 setInterval(autoScroll,7000)
