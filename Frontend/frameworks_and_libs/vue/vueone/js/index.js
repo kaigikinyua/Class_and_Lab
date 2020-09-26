0868473d29be87d8adecda920ae26afeff8bb3b1
kaigikinyua <kaigikinyua@gmail.com>
2020-09-26 23:52:45 +0300

    vue is all I need :)

---
+++ Frontend/frameworks_and_libs/vue/vueone/js/index.js
@@ -0,0 +1,10 @@
+new Vue({
+    el:"#slides",
+    data:{
+        slides:[
+            {"text_content":"This is slide one","image":"./images/food.jpg"},
+            {"text_content":"This is slide two","image":"./images/fruits.jpg"},
+            {"text_content":"This is slide three","image":"./images/pool.jpeg"}
+        ]
+    }
+})
