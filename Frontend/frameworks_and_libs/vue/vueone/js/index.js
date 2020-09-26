23a8c5d79d2fb4d3261ad26d7975e6f46b2e03ff
kaigikinyua <kaigikinyua@gmail.com>
2020-09-27 00:59:12 +0300

    style reviews

--- Frontend/frameworks_and_libs/vue/vueone/js/index.js
+++ Frontend/frameworks_and_libs/vue/vueone/js/index.js
@@ -1,3 +1,16 @@
+var curr_slide=1;
+const slides=document.querySelectorAll('.slide')
+const slides_parent=document.getElementById("slides")
+const screen_width=screen.width
+function auto_scroll(){
+    if(curr_slide<slides.length){curr_slide+=1}
+    else{curr_slide=1}
+    var scroll_by=curr_slide*(screen_width)
+    slides_parent.scrollRight=scroll_by
+    console.log(scroll_by)
+}
+//setInterval(auto_scroll,1000)
+//vue
 new Vue({
     el:"#slides",
     data:{
@@ -7,4 +20,24 @@ new Vue({
             {"text_content":"This is slide three","image":"./images/pool.jpeg"}
         ]
     }
-})
+});
+new Vue({
+    el:"#reviews",
+    data:{
+        reviews:[
+            {"review":"Cool hotel with awsome staff!!","username":"John"},
+            {"review":"Great customer service","username":"James"},
+            {"review":"Great pricing","username":"Marry"},
+            {"review":"Wonderfull people","username":"Sussan"},
+            {"review":"Excellent","username":"Joe"},
+            {"review":"Smashing success","username":"Jim"},
+            {"review":"Love the pool","username":"John"},
+            {"review":"Nice","username":"James"},
+            {"review":"Hello World","username":"Marry"},
+            {"review":"Hello World","username":"Sussan"},
+            {"review":"Hello World","username":"Joe"},
+            {"review":"Hello World","username":"Jim"},
+        ]
+    }
+});
+
