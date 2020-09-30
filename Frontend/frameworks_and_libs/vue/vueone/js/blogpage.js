12a7525ac455a6b258d6a1a845f40d0b99ef23c5
kaigikinyua <kaigikinyua@gmail.com>
2020-09-30 16:11:57 +0300

    blog page init

---
+++ Frontend/frameworks_and_libs/vue/vueone/js/blogpage.js
@@ -0,0 +1,19 @@
+new Vue({
+    el:"#navigation",
+    data:{
+        "bloglist":[
+            {"title":"Branding yourself","author":"Tumbler","date":"12/01/2020"},
+            {"title":"Safe spending","author":"A.A.A","date":"01/01/2019"},
+            {"title":"","author":"Tumbler","date":"13/12/2019"},
+        ]
+    }
+})
+
+new Vue({
+    el:"#blog",
+    data:{
+        "myblogs":[
+            {"title":"","author":"","date":"11/11/2011","paragraphs":[]}
+        ]
+    }
+})
