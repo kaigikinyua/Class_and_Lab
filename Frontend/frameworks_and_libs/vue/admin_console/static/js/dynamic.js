b0cf302d0f29ac21bab32978c928717b6076c77c
kaigikinyua <kaigikinyua@gmail.com>
2020-10-05 15:38:31 +0300

    added random increments

---
+++ Frontend/frameworks_and_libs/vue/admin_console/static/js/dynamic.js
@@ -0,0 +1,18 @@
+window.onload= async ()=>{
+    //setInterval(increment,1000);
+    for(var i=0;i<100;i++){
+        increment();
+        await sleep(5000);
+    }  
+}
+function increment(){
+    var dom_inc=document.querySelectorAll('.increment');
+    dom_inc.forEach(element => {
+        var val=parseInt(element.innerHTML)
+        val+=parseInt(element.dataset.inc)
+        element.innerHTML=val
+    });
+}
+function sleep(time){
+    return new Promise(resolve=>setTimeout(resolve,time));
+}
