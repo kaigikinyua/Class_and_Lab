33ac9d4c600c1c36f4f14e463c88347643a5917e
kaigikinyua <kaigikinyua@gmail.com>
2020-11-12 16:42:54 +0300

    dart futures and async await

--- dart/start/amature.dart
+++ dart/start/amature.dart
@@ -2,16 +2,24 @@
 class MyMethods{
   static void output(String out){print(out);}
   static int increment(int i){return i+1;}
-
+  static Future<String> wait() async{
+    Future.delayed(Duration(seconds: 3));
+    String name="myname";
+    return name;
+  }
 }
 
 
 
-void main(){
+void main() async{
   //coditional operator
   10<100?MyMethods.output("True"):MyMethods.output("false");
   var numbers=[1,2,3,4,5,6];
   numbers.forEach((num)=>{
     print(MyMethods.increment(num)..toString())
   });
+  var name=MyMethods.wait();
+  print(name);
+  print("Done");
+  print(await MyMethods.wait());
 }
