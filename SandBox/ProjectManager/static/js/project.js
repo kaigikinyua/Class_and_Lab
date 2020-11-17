6a03cf0587cbbd0c130ccda600d24ce3348dc67f
kaigikinyua <kaigikinyua@gmail.com>
2020-11-17 04:36:53 +0300

    listing projects done

---
+++ SandBox/ProjectManager/static/js/project.js
@@ -0,0 +1,12 @@
+var projectTiles=new Vue({
+    el:'#app_body',
+    data:{
+        projects:[
+            {"title":"Advertisement Video","complete":false,"expired":false},
+            {"title":"Flutter Application","complete":false,"expired":true},
+            {"title":"React Project","complete":false,"expired":true},
+            {"title":"Frontend Project","complete":true,"expired":false},
+            {"title":"Blender Learning","complete":false,"expired":false},
+        ]
+    }
+});
