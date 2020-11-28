95ad5ed79e97e40e6d7488fc1bb6956acb2cf1dd
kaigikinyua <kaigikinyua@gmail.com>
2020-11-28 23:00:59 +0300

    readblog.dart

---
+++ flutter/buggycode/lib/views/screens/readblog.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+class ReadBlog extends StatefulWidget{
+  String blogTitle;String thumbNail;
+  ReadBlog({this.blogTitle,this.thumbNail});
+  @override
+  State<StatefulWidget> createState()=>_ReadBlog(this.blogTitle,this.thumbNail);
+}
+
+class _ReadBlog extends State<ReadBlog>{
+  String blogTitle;String thumbNail;
+  _ReadBlog(this.blogTitle,this.thumbNail);
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text(this.blogTitle)),
+        body: Container(
+
+        ),
+      );
+    }
+}
+
+
+class Paragraph extends  StatelessWidget {
+  String par;
+  Paragraph(this.par);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(this.par),
+      ),
+    );
+  }
+}
