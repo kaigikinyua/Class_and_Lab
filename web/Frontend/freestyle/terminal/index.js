65c07d2f90323131feaf0fa9392ea9c07274afc7
kaigikinyua <kaigikinyua@gmail.com>
2021-02-05 02:34:12 +0300

    small detour to create a cli on the web

---
+++ web/Frontend/freestyle/terminal/index.js
@@ -0,0 +1,62 @@
+var terminal=document.getElementById("text_term")
+var text=[
+    //create an object for command and result for the output to be more logical
+    "sudo ln -s /usr/lib/x86_64-linux-gnu/libgstreamer-1.0.so.0 /usr/lib/libgstreamer-0.10.so.0",
+    "Hello World",
+    "sudo ln -s /lib/x86_64-linux-gnu/libssl.so.1.0.0 /usr/lib/libssl.so.10",
+    "How about a drink",
+    "ls",
+    "sudo ln -s /lib/x86_64-linux-gnu/libcrypto.so.1.0.0 /usr/lib/libcrypto.so.10",
+    "chmod +x ./scripts/clean.sh",
+    "mv ./videos/test.mkv ./videos/lab",
+    "sudo ln -s /usr/lib/x86_64-linux-gnu/libgstbase-1.0.so.0 /usr/lib/libgstbase-0.10.so.0",
+    "clear",
+    "sudo apt-get install libssl1.0.0 ocl-icd-opencl-dev fakeroot xorriso"
+   
+]
+var lastText=0
+
+function printText(numofLines){
+    for(var j=0;j<numofLines;j++){
+        clearTerminalLines(numofLines)
+        var new_command=text[lastText]
+        var last_command=document.createElement("p")
+        last_command.classList.add("command")
+        terminal.appendChild(last_command)
+        last_command.innerHTML=new_command
+        if(lastText<text.length){lastText+=1}
+        if(lastText>=text.length){lastText=0}
+    }
+}
+function clearTerminalLines(numofLines){
+    var commands=document.querySelectorAll('p')
+    if(commands.length>5){
+        try{
+            terminal.removeChild(commands[0])
+        }catch(e){
+            console.log(`failed to remove ${commands[0]} `)
+            console.log(commands[0])
+        }
+    }
+}
+function typeCommand(command){
+    var new_command=document.createElement("p")
+    new_command.classList.add("command")
+    new_command.classList.add("colored")
+    new_command.innerHTML=`<i class='precur'>user<small>@</small>ironman</i>$ ${command}`
+    terminal.appendChild(new_command)
+
+}
+
+printText(3)
+async function addCharacter({char,commandLine}){
+    var prev_inner=commandLine.innerHTML
+    prev_inner+=char
+    commandLine.innerHTML=prev_inner
+    console.log(`Adding character ${char}`)
+}
+
+setInterval(()=>{
+    printText(Math.floor((Math.random()*10+1)/2))
+    typeCommand(text[lastText].toString())
+},2000)
