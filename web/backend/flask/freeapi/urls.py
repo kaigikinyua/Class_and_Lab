ec9d29f52b13f4a28cac57f7625cdccd4d8994cd
kaigikinyua <kaigikinyua@gmail.com>
2021-02-13 23:21:17 +0300

    cleaning up the useable urls in openapi

--- web/backend/flask/freeapi/urls.py
+++ web/backend/flask/freeapi/urls.py
@@ -14,35 +14,25 @@ def generic_data_page():
 def mydb(action):
     return render_template("mydb.html")
 
-#@app.route('/populatesite')
-#def populateSite():
-#    pass
-
 #api endpoints
 @app.route('/getdata/<genericdata>/<number>')
 def getdata(genericdata,number=100):
     data=GenericData.genericDataActions(genericdata,int(number))
     response={}
     if(data!={} and data!=False and data!=None):
-        response=jsonify(data)
+        response=jsonify({data,"endpoint":"/getdata/{gd}/[number]".format(gd=genericdata)})
     else:
         #run diagnostics on generic data - new thread or process
         #log the error to a file
         response=jsonify({"state":"error","genericdata":{},"message":"There seems to be an error getting generic data {g}".format(g=genericdata)})
     return response
 
-@app.route('/createDB/<randomID>')
-def createDB(randomID):
-    return render_template("index.html")
-
-@app.route('/api/generatedata/<datatype>')
-def api_generatedata(datatype):
-    return jsonify({"message":"not yet implemented"})
-
-@app.route('/api/genericdatalist')
-def api_genericdatalist():
-    datalist={"package":"Open Api","message":"Endpoint under construction"}
-    return jsonify(data)
+"""
+future features
+#mydb
+    --create a json file and customize the populated data
+    --protect file with key
+"""
 
 if __name__=="__main__":
     app.run(debug=True)
