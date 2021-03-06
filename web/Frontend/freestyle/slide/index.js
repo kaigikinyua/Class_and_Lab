c6112d6ee9225a66c588f22a05e2b6a44009048e
kaigikinyua <kaigikinyua@gmail.com>
2021-03-06 07:26:43 +0300

    trying out some resize techniques on the dom

---
+++ web/Frontend/freestyle/slide/index.js
@@ -0,0 +1,53 @@
+var transcription=[
+    {start:100,end:200,text:"Hello World"},
+    {start:201,end:300,text:"Hello World"},
+    {start:301,end:400,text:"Hello World"},
+    {start:401,end:500,text:"Hello World"},
+]
+var mousedown=false
+function renderAdjustable(){
+    var container=document.getElementById('container')
+    transcription.forEach(ts=>{
+        var adjstble=document.createElement('div')
+        adjstble.classList.add('adjustable')
+        adjstble.innerHTML=ts.text
+        adjstble.style.width=(ts.end-ts.start)/1.2
+        adjstble.addEventListener('mousedown',(e)=>{mousedown=true})
+        adjstble.addEventListener('mousemove',adjust)
+        container.appendChild(adjstble)
+    })
+}
+document.addEventListener('mouseup',(e)=>{mousedown=false})
+renderAdjustable()
+function adjust(e){
+    if(mousedown==true){
+        console.log(e.offsetX)
+        console.log(mousedown)
+        var width=e.target.style.width
+        e.target.style.width=parseInt(width)-e.offsetX
+        //console.log(e.target.style.width-e.offsetX)
+    }
+}
+
+
+//option 2
+function renderAdjustable2(){
+    var container=document.getElementById('container2')
+    transcription.forEach(function(ts,i){
+        var adjstble=document.createElement('div')
+        adjstble.classList.add('adjustable')
+        adjstble.innerHTML=ts.text
+        adjstble.style.height=(ts.end-ts.start)/1.2
+        adjstble.style.dragable=true
+        adjstble.dataset['index']=i
+        //adjstble.style.resize='horizontal'
+        new ResizeObserver(resizeAdjustable).observe(adjstble)
+        container.appendChild(adjstble)
+    })
+}
+renderAdjustable2()
+function resizeAdjustable(element){
+    var index=element[0].target.dataset.index
+    transcription[index].end=parseInt(element[0].target.style.height)*2
+    console.log(element[0].target.style.height)
+}
