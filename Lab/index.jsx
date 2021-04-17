f74051531224d308a249531b47f9fd43830d039c
kaigikinyua <kaigikinyua@gmail.com>
2021-04-17 23:25:07 +0300

    differentiating between React.Compoent and React.createClass

--- Lab/index.jsx
+++ Lab/index.jsx
@@ -15,5 +15,24 @@ var ClickComponent=React.createClass({
         )
     }
 });
-ReactDOM.render(<ClickComponent clicks="10"/>,document.getElementById('clickComponent'))
+class ClickComponent2 extends React.Component{
+    constructor(props,context){
+        super(props,context)
+        this.state={clicks:parseInt(this.props.clicks)}
+        this.incrementClicks=this.incrementClicks.bind(this)
+    }
+    incrementClicks(){
+        console.log(this.state)
+        this.setState((prevState)=>{return {clicks:prevState.clicks+1}})
+    }
+    render(){
+        return(
+            <div className="click">
+                <button onClick={this.incrementClicks}>{this.state.clicks}</button>
+            </div>
+        )
+    }
+}
 
+ReactDOM.render(<ClickComponent clicks="10"/>,document.getElementById('clickComponent'))
+ReactDOM.render(<ClickComponent2 clicks="100"/>,document.getElementById('clickComponent2'));
