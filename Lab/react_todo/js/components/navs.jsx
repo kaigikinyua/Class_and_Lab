22d26141961308e1671cd2e8e5dd688399090495
kaigikinyua <kaigikinyua@gmail.com>
2021-04-27 22:24:58 +0300

    topnav and link for the taskflow

--- Lab/react_todo/js/components/navs.jsx
+++ Lab/react_todo/js/components/navs.jsx
@@ -10,3 +10,39 @@ class UserPageNav extends React.Component{
         </div>
     }
 }
+
+class TopNav extends React.Component{
+    constructor(props,context){
+        super(props,context)
+    }
+    render(){
+        var links=this.props.links.forEach(e => {
+            return <Link name={e.name} link={e.link}/>
+        });
+        return(
+            <div className="topnav">
+                <div className="banner">
+                    <h3 className="banner">{this.props.title}</h3>
+                </div>
+                <div className="links">
+                    <ul className="links">
+                        {links}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Link extends React.Component{
+    constructor(props,context){
+        super(props,context)
+    }
+    render(){
+        return (
+            <a href={this.props.link} className="link">
+                <span>{this.props.name}</span>
+            </a>
+        )
+    }
+}
