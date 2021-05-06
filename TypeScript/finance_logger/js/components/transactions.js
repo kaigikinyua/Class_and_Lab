cd4d718a2ebb8aabc787ab129504b900e2e75aaa
kaigikinyua <kaigikinyua@gmail.com>
2021-05-06 16:12:21 +0300

    finance logger done: TypeScript is awsome

--- TypeScript/finance_logger/js/components/transactions.js
+++ TypeScript/finance_logger/js/components/transactions.js
@@ -11,13 +11,16 @@ var Transaction = /** @class */ (function () {
         container.innerHTML = "<h4>" + this.title + "</h4>\n                        <div class='desc'>\n                            <span class='desc'>" + this.desc + "</span>\n                        </div>";
         container.addEventListener('click', function (e) {
             var open = container.getAttribute('data-open');
+            var psudo = window.getComputedStyle(container.children[0], ':after');
             if (open == '0') {
                 container.children[1].setAttribute('style', 'display:block;');
                 container.setAttribute('data-open', '1');
+                psudo.content = '';
             }
             else {
                 container.children[1].setAttribute('style', 'display:none;');
                 container.setAttribute('data-open', '0');
+                psudo.content = '';
             }
         });
         return container;
--- TypeScript/finance_logger/src/components/transactions.ts
+++ TypeScript/finance_logger/src/components/transactions.ts
@@ -14,12 +14,15 @@ export class Transaction{
                         </div>`
         container.addEventListener('click',e=>{
             var open:string=container.getAttribute('data-open') as string
+            var psudo=window.getComputedStyle(container.children[0],':after')
             if(open=='0'){
                 container.children[1].setAttribute('style','display:block;')
                 container.setAttribute('data-open','1')
+                psudo.content=''
             }else{
                 container.children[1].setAttribute('style','display:none;')
                 container.setAttribute('data-open','0')
+                psudo.content=''
             }
         })
         return container
