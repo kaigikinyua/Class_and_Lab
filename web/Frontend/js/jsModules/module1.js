f40b0f099eda496c607e77c0ae2e84bb404ab25a
kaigikinyua <kaigikinyua@gmail.com>
2021-05-29 21:23:44 +0300

    global and local variable scope, quite helpfull insignt in modulerizing the code

---
+++ web/Frontend/js/jsModules/module1.js
@@ -0,0 +1,14 @@
+var baseUrl="https://domain.com"
+window.PaintClass=class PaintClass{
+    constructor(){
+        console.log("Hello from PaintClass")
+       // alert("Paint class")
+    }
+}
+var Utils={
+    Messages:{
+        error:(message)=>{
+            alert(`error ${message}`)
+        }
+    }
+}
